4df7fa4d683bcc78d68d95c0f0dfc35ee4fb2201
idontfeelsogoood <msitko21@interia.pl>
2023-05-27 19:35:48 +0200

    deleting custom csvParser using builtint

--- main_page.py
+++ main_page.py
@@ -5,19 +5,15 @@ import pandas as pd
 import logging
 import numpy as np
 
-from csv_processor import CSVDataProcessor
 
-def home_page(data_csv:"CSVDataProcessor"):
+def home_page(data):
     st.title("Home Page")
-
-    chart_data = data_csv.data[['bpm','nrgy']]
+    chart_data = data[["bpm","nrgy"]]
     st.line_chart(chart_data)
 
-
 def main():
-    data = CSVDataProcessor("csvs/spotify_2010_2019_data.csv")
-    data.load_csv()
-    home_page(data)
+    df = pd.read_csv("csvs/spotify_2010_2019_data.csv")
+    home_page(df)
 
 main()
 
