9bc2f404fd217964033cb237fef24b9448b13eb4
marcin sitko <marcin.sitko@nokia.com>
2023-05-29 11:31:13 +0200

    testing branch with no sudo

--- main_page.py
+++ main_page.py
@@ -6,13 +6,12 @@ import pandas as pd
 import logging
 import numpy as np
 
-
-#IMPORTANT!
+# IMPORTANT!
 st.set_page_config(layout='wide')
-#-----
+
 
 @st.cache_data
-def load_data(path:str) -> pd.DataFrame:
+def load_data(path: str) -> pd.DataFrame:
     try:
         df = pd.read_csv(path)
         return df
@@ -38,27 +37,27 @@ def home_page(data):
         width=1000,
         height=500
     )
-    st.plotly_chart(fig,theme=None)
+    st.plotly_chart(fig, theme=None)
 
     fig1 = go.Figure(
-        data=go.Surface(z= data[["bpm", "nrgy"]]),
+        data=go.Surface(z=data[["bpm", "nrgy"]]),
         layout=go.Layout(
             title="+100 do zajebistosci",
             width=1000,
             height=800,
         ))
-    st.plotly_chart(fig1,theme=None)
+    st.plotly_chart(fig1, theme=None)
 
     st.header("Bar Chart")
     st.bar_chart(chart_data)
 
-    with st.expander(label="Wykres bpm i nrgy",expanded=True):
+    with st.expander(label="Wykres bpm i nrgy", expanded=True):
         tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
         tab1.line_chart(chart_data)
         tab2.dataframe(data)
-        #st.line_chart(chart_data)
+        # st.line_chart(chart_data)
 
-    col1,col2 = st.columns(2,gap="medium")
+    col1, col2 = st.columns(2, gap="medium")
 
     with col1:
         with st.expander(label="left", expanded=True):
@@ -72,19 +71,10 @@ def home_page(data):
             tab1.area_chart(chart_data)
             tab2.dataframe(data)
 
+
 def main():
     data = load_data("csvs/spotify_2010_2019_data.csv")
     home_page(data)
 
+
 main()
-
-
-
-
-
-
-
-
-
-
-
