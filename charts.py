1981f7b97917e0d0c06dcc9226495c37257286e7
idontfeelsogoood <msitko21@interia.pl>
2023-06-04 19:25:06 +0200

    dodanie nowych tabow do kazdego expandera z wykresem prezentujace inne wykresy

--- charts.py
+++ charts.py
@@ -4,7 +4,9 @@ import plotly.graph_objects as go
 import pandas as pd
 from spotifySt import search_track, search_artist
 from spotifySt import *
+import altair
 
+TYPES_OF_ST_CHARTS = ['line','bar','area','scatter','map']
 
 def spotifyPlayer(song_title):
     # Spotify Snippet Player
@@ -43,7 +45,6 @@ def spotifyAlbumPicture(album_title, custom_width=None):
             return None
 
 
-
 def chart_popularity_genre(data):
     # A chart depicting the relationship between 'top genre'  and 'Popularity'
     fig_popularity_genre = px.bar(data_frame=data,
@@ -71,15 +72,42 @@ def chart_popularity_genre(data):
     st.plotly_chart(fig_popularity_genre, theme=None)
 
 
-def chart_bpm_year(data):
+def chart_bpm_year(data,type_of_chart="bar"):
     # A chart depicting the relationship between 'Year'  and 'BPM'
     df_mean_bpm = data.groupby('year')['bpm'].mean().reset_index()
-    fig_bpm_year = px.bar(data_frame=df_mean_bpm,
-                          x='year',
-                          y='bpm',
-                          title='Chart Showing Average Tempo of Songs by Year',
-                          color='bpm'
-                          )
+    fig_bpm_year = None
+
+    if type_of_chart in TYPES_OF_ST_CHARTS:
+        pass
+    else:
+        return st.write(f"No such a chart like {type_of_chart}")
+
+    if type_of_chart=="bar":
+        fig_bpm_year = px.bar(data_frame=df_mean_bpm,
+                              x='year',
+                              y='bpm',
+                              title='Chart Showing Average Tempo of Songs by Year',
+                              color='bpm'
+                              )
+
+    elif type_of_chart == "line":
+        fig_bpm_year = px.line(data_frame=df_mean_bpm,
+                               x='year',
+                               y='bpm',
+                               title='Chart Showing Average Tempo of Songs by Year',
+                               )
+
+    elif type_of_chart == "area":
+        return  st.area_chart(data=df_mean_bpm,x='year',y='bpm',use_container_width=True)
+
+    elif type_of_chart=="scatter":
+        fig_bpm_year = px.scatter(data_frame=df_mean_bpm,
+                                  x='year',
+                                  y='bpm',
+                                  title='Chart Showing Average Tempo of Songs by Year',
+                                  size='bpm',
+                                  color='bpm',
+                                  log_y=True)
 
     fig_bpm_year.update_layout(
         xaxis={'title': '', 'tickfont': {'size': 13}},
@@ -89,14 +117,41 @@ def chart_bpm_year(data):
     return st.plotly_chart(fig_bpm_year, theme=None, use_container_width=True), df_mean_bpm
 
 
-def chart_genre_nrgy(data):
+def chart_genre_nrgy(data,type_of_chart="bar"):
     # A chart depicting the relationship between 'top genre' and 'energy'
     df_mean_nrgy = data.groupby('top genre')['nrgy'].mean().reset_index()
-    fig_genre_nrgy = px.bar(data_frame=df_mean_nrgy,
-                            x='top genre',
-                            y='nrgy',
-                            title='Chart Showing Average Energy of Songs by Their Genre',
-                            color='top genre')
+    fig_genre_nrgy = None
+
+    if type_of_chart in TYPES_OF_ST_CHARTS:
+        pass
+    else:
+        return st.write(f"No such a chart like {type_of_chart}")
+
+    if type_of_chart == "bar":
+        fig_genre_nrgy = px.bar(data_frame=df_mean_nrgy,
+                                x='top genre',
+                                y='nrgy',
+                                title='Chart Showing Average Energy of Songs by Their Genre',
+                                color='top genre')
+
+    elif type_of_chart == "line":
+        fig_genre_nrgy = px.line(data_frame=df_mean_nrgy,
+                               x='top genre',
+                               y='nrgy',
+                                title='Chart Showing Average Energy of Songs by Their Genre',
+                               )
+
+    elif type_of_chart == "area":
+        return  st.area_chart(data=df_mean_nrgy,x='top genre',y='nrgy',use_container_width=True)
+
+    elif type_of_chart=="scatter":
+        fig_genre_nrgy = px.scatter(data_frame=df_mean_nrgy,
+                                  x='top genre',
+                                  y='nrgy',
+                                  title='Chart Showing Average Energy of Songs by Their Genre',
+                                  size='nrgy',
+                                  color='top genre',
+                                  log_y=True)
 
     fig_genre_nrgy.update_layout(
         xaxis={'title': '', 'tickfont': {'size': 13}, 'tickangle': 45},
@@ -106,15 +161,42 @@ def chart_genre_nrgy(data):
     return st.plotly_chart(fig_genre_nrgy, theme=None, use_container_width=True), df_mean_nrgy
 
 
-def chart_year_dnce(data):
+def chart_year_dnce(data,type_of_chart="bar"):
     # A chart depicting the relationship between 'dnce' and 'year'
     df_mean_dnce = data.groupby('year')['dnce'].mean().reset_index()
-    fig_year_dnce =  px.bar(data_frame=df_mean_dnce,
-                            x='year',
-                            y='dnce',
-                            title="Chart Showing Average Danceability of Songs by Their year",
-                            color='dnce',
-                            log_y=True)
+    fig_year_dnce = None
+
+    if type_of_chart in TYPES_OF_ST_CHARTS:
+        pass
+    else:
+        return st.write(f"No such a chart like {type_of_chart}")
+
+    if type_of_chart == "bar":
+        fig_year_dnce =  px.bar(data_frame=df_mean_dnce,
+                                x='year',
+                                y='dnce',
+                                title="Chart Showing Average Danceability of Songs by Their year",
+                                color='dnce',
+                                log_y=True)
+
+    elif type_of_chart == "line":
+        fig_year_dnce = px.line(data_frame=df_mean_dnce,
+                               x='year',
+                               y='dnce',
+                               title="Chart Showing Average Danceability of Songs by Their year",
+                               )
+
+    elif type_of_chart == "area":
+        return  st.area_chart(data=df_mean_dnce,x='year',y='dnce',use_container_width=True)
+
+    elif type_of_chart=="scatter":
+        fig_year_dnce = px.scatter(data_frame=df_mean_dnce,
+                                  x='year',
+                                  y='dnce',
+                                  title="Chart Showing Average Danceability of Songs by Their year",
+                                  size='dnce',
+                                  color='year',
+                                  log_y=True)
 
     fig_year_dnce.update_layout(
         xaxis={'title': '', 'tickfont': {'size': 13}},
@@ -122,14 +204,34 @@ def chart_year_dnce(data):
     )
     return st.plotly_chart(fig_year_dnce, theme=None, use_container_width=True), df_mean_dnce
 
-def chart_val_year(data):
+def chart_val_year(data,type_of_chart="line"):
     # A chart depicting the relationship between 'val' and 'year'
-    df_mean_val = abs(data.groupby('year')['val'].mean().reset_index())
-    fig_val_year = px.line(data_frame=df_mean_val,
-                          x='year',
-                          y='val',
-                          title="Chart Showing Average VAL of Songs by their Genre"
-                          )
+    df_mean_val = data.groupby('year')['val'].mean().reset_index()
+    fig_val_year=None
+
+    if type_of_chart in TYPES_OF_ST_CHARTS:
+        pass
+    else:
+        return st.write(f"No such a chart like {type_of_chart}")
+
+    if type_of_chart == "line":
+        fig_val_year = px.line(data_frame=df_mean_val,
+                               x='year',
+                               y='val',
+                               title="Chart Showing Average Danceability of Songs by Their year",
+                               )
+
+    elif type_of_chart == "area":
+        return  st.area_chart(data=df_mean_val,x='year',y='val',use_container_width=True)
+
+    elif type_of_chart=="scatter":
+        fig_val_year = px.scatter(data_frame=df_mean_val,
+                                  x='year',
+                                  y='val',
+                                  title="Chart Showing Average Danceability of Songs by Their year",
+                                  size='val',
+                                  color='val',
+                                  log_y=True)
 
     fig_val_year.update_layout(
         xaxis={'title': '', 'tickfont': {'size': 13},'tickangle':45},
